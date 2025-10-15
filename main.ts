94b6f4211eaf88f32a58a06411f56bd072fa2a5b
Ian Too <ianktoo@gmail.com>
2025-10-15 10:21:53 -0700

    Added comments

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+/**
+ * This is a comment:
+ * 
+ * This Micro:bit code uses AI model to detect movement and perform an action.
+ * 
+ * The actions that we have here are:
+ * 
+ * 1. Clap
+ * 
+ * 2. Wave
+ * 
+ * 3. Dance
+ * 
+ * 4. Rest
+ * 
+ * 5. Unknown Event - The Micro:bit is not aware of this action. You haven't taught it.
+ * 
+ * Our goal is to record how many times we do a certain action.
+ * 
+ * We are going to save the action in variables
+ */
 ml.onStart(ml.event.Dance, function () {
     basic.showIcon(IconNames.No)
 })
