cd9977fe2535e490d33e81000c9acf472e60f117
Ian Too <ianktoo@gmail.com>
2025-10-15 09:51:48 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+ml.onStart(ml.event.Dance, function () {
+    basic.showIcon(IconNames.No)
+})
+ml.onStart(ml.event.Clap, function () {
+    basic.showIcon(IconNames.Cow)
+})
+ml.onStart(ml.event.Wave, function () {
+    basic.showIcon(IconNames.Chessboard)
+})
+ml.onStart(ml.event.Rest, function () {
+    basic.showIcon(IconNames.Square)
+})
